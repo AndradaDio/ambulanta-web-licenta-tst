d1508ffe1ecf6042f3a91c569e89bc91fcc1e010
Andrada Dioszegi <151720343+AndradaDio@users.noreply.github.com>
2026-06-14 20:55:31 +0300

    Create main.jsx

---
+++ ambulanta-web/src/main.jsx
@@ -0,0 +1,11 @@
+// Punctul de intrare al aplicatiei React
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
